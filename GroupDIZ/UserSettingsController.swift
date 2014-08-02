d1d3353e310525cdd5a7aa0344dd0bcbae39bdfb
serco-chen <xg.chen87@gmail.com>
2014-08-02 17:14:23 +0800

    add post and get identifier request

--- GroupDIZ/UserSettingsController.swift
+++ GroupDIZ/UserSettingsController.swift
@@ -8,10 +8,28 @@
 
 import UIKit
 
-class UserSettingsController: UIViewController {
+class UserSettingsController: UIViewController, APIServiceDelegate {
+    
+    var apiService: APIService = APIService()
+    
+    @IBOutlet weak var nickNameField: UITextField!
+    @IBAction func SubmitNickName(sender: AnyObject) {
+        if let nickName = nickNameField.text {
+            let identifierForVendor = UIDevice.currentDevice().identifierForVendor
+            let identifier = identifierForVendor.UUIDString
+            var data = NSMutableData()
+            data.appendData("nickname=\(nickName)&identifier=\(identifier)".dataUsingEncoding(NSUTF8StringEncoding))
+            apiService.onSub("http://zuoyouba.com/api/v0/1/users", method: "Post", data:data)
+        }
+    }
+    
+    func didReceiveResults(data: NSDictionary) {
+
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        apiService.delegate = self
         let identifier = UIDevice.currentDevice().respondsToSelector(Selector("identifierForVendor"))
         
         // Do any additional setup after loading the view, typically from a nib.
