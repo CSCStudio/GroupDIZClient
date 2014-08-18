24ecf9a399ad6f23fe52b4ca5c78a7eb34bfc2d6
songjiayang <songjiayang@weidaxue.me>
2014-08-18 22:54:51 +0800

    add vote action

--- GroupDIZ/ShowPointController.swift
+++ GroupDIZ/ShowPointController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ShowPointController: UIViewController {
+class ShowPointController: UIViewController, APIServiceDelegate {
     
     // MARK: Properties
     var pointData: NSDictionary!
+    var apiService: APIService = APIService()
     
     @IBOutlet weak var pointUser: UILabel!
     @IBOutlet weak var createAt: UILabel!
@@ -21,6 +22,7 @@ class ShowPointController: UIViewController {
     // MARK: Override View Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        apiService.delegate = self
         self.pointTitle.text = self.pointData.objectForKey("title")  as NSString
         self.pointUser.text = self.pointData.objectForKey("username")  as NSString
         self.createAt.text = self.pointData.objectForKey("created_at")  as NSString
@@ -32,6 +34,31 @@ class ShowPointController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func disagree(sender: UIBarButtonItem) {
+        doVote("disagree")
+        
+    }
+    
+    @IBAction func agree(sender: UIBarButtonItem) {
+        doVote("agree")
+    }
+    
+    func doVote(kind: String) {
+        let id = self.pointData.objectForKey("id") as Int
+        let parameters = ["kind": kind,  "identifier": APIService.identifier]
+        apiService.post("/points/\(id)/votes", parameters:parameters)
+    }
+    
+    // MARK: Delegate Functions
+    func didReceiveResults(response: NSDictionary) {
+        let alertView = UIAlertView(title: "Success", message: "Voted Successful", delegate: self, cancelButtonTitle: "OK")
+        alertView.show()
+    }
+    
+    func didReceiveError(description: String) {
+        let alertView = UIAlertView(title: "Error", message: description, delegate: self, cancelButtonTitle: "OK")
+        alertView.show()
+    }
     
 }
 
